da3b166aa65f26a67e47841ff34080ab7dc04892
Isadora Pilau <isadorapda@gmail.com>
2023-05-03 15:27:29 +0100

    test: 💍 complete E2E tests

--- test/transactions.test.ts
+++ test/transactions.test.ts
@@ -1,15 +1,19 @@
-import { test, beforeAll, afterAll, describe } from 'vitest'
+import { expect, test, beforeAll, afterAll, describe, beforeEach } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
+import { execSync } from 'node:child_process'
 
 describe('Transactions Routes', () => {
   beforeAll(async () => {
-    // we are using this here because of the fact that fastify plugins are all asynchronous, so we need to wait the server to start and then test it.
-    // without this the test fails because it doesn't find the routes.
     await app.ready()
   })
   afterAll(async () => {
     await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback')
+    execSync('npm run knex migrate:latest')
   })
 
   test('user should be able to create a new transaction', async () => {
@@ -21,5 +25,81 @@ describe('Transactions Routes', () => {
         type: 'credit',
       })
       .expect(201)
+  })
+  test('user should be able to access a list of transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Test transaction 01',
+        amount: 500,
+        type: 'credit',
+      })
+    const cookie = createTransactionResponse.get('Set-Cookie')
+    const listTransactionResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookie)
+      .expect(200)
+
+    expect(listTransactionResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'Test transaction 01',
+        amount: 500,
+      }),
+    ])
+  })
+
+  test('user should be able to access a specific transaction', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Test transaction 01',
+        amount: 500,
+        type: 'credit',
+      })
+    const cookie = createTransactionResponse.get('Set-Cookie')
+    const listTransactionResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookie)
+      .expect(200)
+
+    const transactionId = listTransactionResponse.body.transactions[0].id
+
+    const getTransactionResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookie)
+      .expect(200)
+
+    expect(getTransactionResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: 'Test transaction 01',
+        amount: 500,
+      }),
+    )
+  })
+
+  test('user should be able to access the summary of transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Credit transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+    const cookie = createTransactionResponse.get('Set-Cookie')
+
+    await request(app.server).post('/transactions').set('Cookie', cookie).send({
+      title: 'Debit transaction',
+      amount: 2000,
+      type: 'debit',
+    })
+
+    const summaryResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookie)
+      .expect(200)
+
+    expect(summaryResponse.body.summaryTransactions).toEqual({
+      amount: 3000,
+    })
   })
 })
